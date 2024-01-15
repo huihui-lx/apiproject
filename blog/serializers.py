e3416da507d074675418fb68a9ee9b47b434fbfa
liuxin <1652020353@qq.com>
2024-01-15 17:03:21 +0800

    以DRF序列化器和函数视图开发API（1）

---
+++ blog/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import Article
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+''' 使用Serializer类
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, allow_blank=True, max_length=90)
+    body = serializers.CharField(required=False, allow_blank=True)
+    author = serializers.ReadOnlyField(source="author.id")
+    status = serializers.ChoiceField(choices=Article.STATUS_CHOICES, default='p')
+    create_date = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        """
+        Create a new "article" instance
+        """
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Use validated data to return an existing `Article`instance。"""
+        instance.title = validated_data.get('title', instance.title)
+        instance.body = validated_data.get('body', instance.body)
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+        return instance
+'''
+
+# 使用ModelSerializer类
+"""
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+        read_only_fields = ('id', 'author', 'create_date')
+"""
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+        read_only_fields = ('id', 'create_date')
